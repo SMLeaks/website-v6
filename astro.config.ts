833bc6e653242a40602f74bf1d49675fb9d4108f
trbodev <55338035+trbodev@users.noreply.github.com>
2023-05-01 16:35:28 -0400

    feat: add seo

--- astro.config.ts
+++ astro.config.ts
@@ -2,10 +2,25 @@
 import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
 
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            URL?: string,
+            CF_PAGES_URL?: string
+        }
+    }
+}
+
+
+
 export default defineConfig({
-  integrations: [
-    UnoCSS({
-        injectReset: true
-    }),
-  ],
+    experimental: {
+        assets: true
+    },
+    site: process.env.URL ?? process.env.CF_PAGES_URL ?? 'http://localhost:3000',
+    integrations: [
+        UnoCSS({
+            injectReset: true
+        }),
+    ],
 });
