c94041da6ca7a5ee524a726c81b3b2dd49c47d0b
trbodev <55338035+trbodev@users.noreply.github.com>
2023-05-01 18:45:24 -0400

    feat: update article page

--- markdoc.config.ts
+++ markdoc.config.ts
@@ -1,9 +1,12 @@
 //@ts-ignore
 import type { ConfigType as MarkdocConfig } from '@markdoc/markdoc';
+import Markdoc from '@markdoc/markdoc';
 //@ts-ignore
 import { defineMarkdocConfig } from '@astrojs/markdoc/config';
 //@ts-ignore
 import Heading from '@components/Markdown/Heading.astro';
+//@ts-ignore
+import Image from '@components/Markdown/Image.astro';
 
 export default defineMarkdocConfig({
     tags: {
@@ -19,6 +22,13 @@ export default defineMarkdocConfig({
                 }
             },
             render: Heading
+        },
+        image: {
+            attributes: {
+				...Markdoc.nodes.image.attributes,
+				__optimizedSrc: { type: 'Object' },
+            },
+            render: Image
         }
     }
 } as MarkdocConfig);
